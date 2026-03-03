5aceb5a4a565786c078956dce99eac1260ae6b05
Hetvi Patel <hetvipatel2302@gmail.com>
2026-03-03 11:06:44 +0000

    Fields

--- workboard/workboard/patches/v15_0/set_wb_task_permlevel_property_setters.py
+++ workboard/workboard/patches/v15_0/set_wb_task_permlevel_property_setters.py
@@ -5,10 +5,16 @@ import frappe
 
 def execute():
 	"""Set permlevel = 1 for all WB Task fields via Property Setters,
-	except for proof_of_work and task_completion_remark which stay at permlevel 2."""
+	except: proof_of_work and task_completion_remark (permlevel 2),
+	and checklist_template and wb_task_checklist_details (permlevel 0)."""
 
 	doctype = "WB Task"
-	exclude_fields = {"proof_of_work", "task_completion_remark"}
+	exclude_fields = {
+		"proof_of_work",
+		"task_completion_remark",
+		"checklist_template",
+		"wb_task_checklist_details",
+	}
 
 	doc = frappe.get_doc("DocType", doctype)
 
@@ -42,6 +48,31 @@ def execute():
 				}
 			)
 			ps.insert(ignore_permissions=True)
+
+	# Ensure permlevel = 0 for checklist fields (excluded from permlevel 1 above)
+	for fieldname in ("checklist_template", "wb_task_checklist_details"):
+		existing_name = frappe.db.exists(
+			"Property Setter",
+			{"doc_type": doctype, "field_name": fieldname, "property": "permlevel"},
+		)
+		if existing_name:
+			ps = frappe.get_doc("Property Setter", existing_name)
+			ps.value = "0"
+			ps.property_type = "Int"
+			ps.doctype_or_field = "DocField"
+			ps.save(ignore_permissions=True)
+		else:
+			frappe.get_doc(
+				{
+					"doctype": "Property Setter",
+					"doctype_or_field": "DocField",
+					"doc_type": doctype,
+					"field_name": fieldname,
+					"property": "permlevel",
+					"value": "0",
+					"property_type": "Int",
+				}
+			).insert(ignore_permissions=True)
 
 	frappe.db.commit()
 
