3eae4e6e7de46cbe69be14a6a0e946e3d7843ac2
Hetvi Patel <hetvipatel2302@gmail.com>
2026-03-11 09:20:13 +0000

    Checklist template

--- workboard/workboard/doctype/wb_task_checklist_template/wb_task_checklist_template.js
+++ workboard/workboard/doctype/wb_task_checklist_template/wb_task_checklist_template.js
@@ -3,29 +3,39 @@
 
 frappe.ui.form.on("WB Task Checklist Template", {
 	refresh(frm) {
-		let grid = frm.fields_dict.wb_task_checklist_template_details && frm.fields_dict.wb_task_checklist_template_details.grid;
-		if (grid) {
-			let df = grid.get_field("verification_field").df;
-			df.fieldtype = "Select";
-			df.options = "";
-			grid.refresh();
+		let table_field = "wb_task_checklist_template_details";
+		let fieldname = "verification_field";
+
+		if (frm.fields_dict && frm.fields_dict[table_field] && frm.fields_dict[table_field].grid) {
+			let grid = frm.fields_dict[table_field].grid;
+			let field = grid.get_field(fieldname);
+			if (field && field.df) {
+				field.df.fieldtype = "Select";
+				field.df.focus_select = true;
+				grid.refresh();
+			}
 		}
 
 		// Show fieldname instead of legacy DocField hash: preload map and set formatter
-		var verification_values = (frm.doc.wb_task_checklist_template_details || [])
-			.map(function(r) { return r.verification_field; })
+		let verification_values = (frm.doc[table_field] || [])
+			.map(function(r) { return r[fieldname]; })
 			.filter(Boolean);
+
 		if (verification_values.length) {
 			frappe.call({
 				method: "workboard.utils.get_docfield_fieldnames",
 				args: { docfield_names: verification_values },
 				callback: function(r) {
-					var map = r.message || {};
+					let map = r.message || {};
+					let grid = frm.fields_dict[table_field] && frm.fields_dict[table_field].grid;
 					if (grid) {
-						grid.get_field("verification_field").formatter = function(value) {
-							return map[value] || value;
-						};
-						grid.refresh();
+						let field = grid.get_field(fieldname);
+						if (field) {
+							field.formatter = function(value) {
+								return map[value] || value;
+							};
+							grid.refresh();
+						}
 					}
 				}
 			});
@@ -43,7 +53,7 @@ frappe.ui.form.on("WB Task Checklist Template Details", {
 	on_row_refresh: function(frm, cdt, cdn) {
 		set_verification_field_options(frm, cdt, cdn);
 	},
-	// Handle focus/click in the grid
+	// Handle interaction in the grid
 	verification_field: function(frm, cdt, cdn) {
 		set_verification_field_options(frm, cdt, cdn);
 	}
@@ -60,18 +70,13 @@ function set_verification_field_options(frm, cdt, cdn) {
 			if (r.message && r.message.length) {
 				let options = [""].concat(r.message.map(function(f) { return f.value; }));
 				
-				// Safely update the field property for THIS specific row
-				// Using the standard frm.set_df_property which is the correct way for row-specific options
+				// Standard way to set row-specific options
 				try {
-					frm.set_df_property("verification_field", "options", options, frm.doc.name, "wb_task_checklist_template_details", cdn);
-				} catch (e) {
-					// Fallback if set_df_property fails
-					let grid = frm.fields_dict.wb_task_checklist_template_details.grid;
-					let field = grid.get_field("verification_field");
-					if (field) {
-						field.df.options = options;
-						grid.refresh_field("verification_field");
+					if (frm && frm.set_df_property) {
+						frm.set_df_property("verification_field", "options", options, frm.doc.name, "wb_task_checklist_template_details", cdn);
 					}
+				} catch (e) {
+					console.log("Error setting options via frm.set_df_property", e);
 				}
 			}
 		}
