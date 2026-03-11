7a88c3b7d128be3c1e4e420a12b44bf8f6fe1d3f
Hetvi Patel <hetvipatel2302@gmail.com>
2026-03-11 08:32:38 +0000

    Checklist template

--- workboard/workboard/doctype/wb_task_checklist_template/wb_task_checklist_template.js
+++ workboard/workboard/doctype/wb_task_checklist_template/wb_task_checklist_template.js
@@ -25,46 +25,33 @@ frappe.ui.form.on("WB Task Checklist Template", {
 		} else {
 			grid.get_field("verification_field").formatter = null;
 		}
-
-		// Open field selector dialog when user focuses Verification Field (if doctype set)
-		grid.form.on("verification_field", "focus", function() {
-			var row = grid.form.get_selected_row();
-			if (!row || !row.verification_doctype) return;
-			open_verification_field_dialog(frm, grid, row);
-		});
 	}
 });
 
-function open_verification_field_dialog(frm, grid, row) {
+frappe.ui.form.on("WB Task Checklist Template Details", {
+	verification_doctype: function(frm, cdt, cdn) {
+		set_verification_field_options(frm, cdt, cdn);
+	},
+	form_render: function(frm, cdt, cdn) {
+		set_verification_field_options(frm, cdt, cdn);
+	},
+	on_row_refresh: function(frm, cdt, cdn) {
+		set_verification_field_options(frm, cdt, cdn);
+	}
+});
+
+function set_verification_field_options(frm, cdt, cdn) {
+	let row = locals[cdt][cdn];
+	if (!row.verification_doctype) return;
+
 	frappe.call({
 		method: "workboard.utils.get_doctype_fields",
 		args: { doctype: row.verification_doctype },
 		callback: function(r) {
-			var options = r.message || [];
-			if (!options.length) {
-				frappe.msgprint(__("No fields found for {0}", [row.verification_doctype]));
-				return;
+			if (r.message && r.message.length) {
+				let options = r.message.map(function(f) { return f.value; });
+				frm.set_df_property("verification_field", "options", [""].concat(options), frm.doc.name, "wb_task_checklist_template_details", cdn);
 			}
-			var d = new frappe.ui.Dialog({
-				title: __("Select Verification Field"),
-				fields: [{
-					fieldtype: "Select",
-					fieldname: "fieldname",
-					label: __("Field"),
-					options: options.map(function(o) { return o.label + " (" + o.value + ")"; }),
-					onchange: function() {
-						var val = this.value;
-						if (val) {
-							var fieldname = val.replace(/^.*\(([^)]+)\)$/, "$1");
-							row.verification_field = fieldname;
-							grid.refresh();
-							frm.dirty();
-							d.hide();
-						}
-					}
-				}]
-			});
-			d.show();
 		}
 	});
 }
